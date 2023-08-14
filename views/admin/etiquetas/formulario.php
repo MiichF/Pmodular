33c39596c16af4ea51ea34acf68905f8cb5d7884
DarkDucking <segundoleoshadow@gmail.com>
2023-08-14 08:11:10 -0600

    formularioSCSS
    SCSS para normalizar los formularios

---
+++ views/admin/etiquetas/formulario.php
@@ -0,0 +1,26 @@
+<fieldset class="formulario__fieldset">
+    <legend class="formulario__legend">Datos de la etiqueta</legend>
+
+    <div class="formulario__campo">
+        <label for="nombre" class="formulario__label">Nombre de la etiqueta</label>
+        <input 
+            type="text"
+            class="formulario__input"
+            id="nombre"
+            name="nombre"
+            placeholder="Nombre etiqueta"
+            value="<?php echo $etiqueta->nombre ?? ''; ?>"
+        >
+    </div>
+    <div class="formulario__campo">
+        <label for="prefijo" class="formulario__label">Prefijo de la Etiqueta</label>
+        <input 
+            type="text"
+            class="formulario__input"
+            id="prefijo"
+            name="prefijo"
+            placeholder="Prefijo de la etiqueta"
+            value="<?php echo $etiqueta->nombre ?? ''; ?>"
+        >
+    </div>
+</fieldset>
--- views/admin/etiquetas/index.php
+++ views/admin/etiquetas/index.php
@@ -1,1 +1,12 @@
+<<<<<<< Updated upstream
 <h2 class="dashboard__heading"><?php echo $titulo; ?></h2>
+=======
+<h2 class="dashboard__heading"><?php echo $titulo; ?></h2>
+
+<div class="dashboard__contenedor-boton">
+    <a href="/admin/etiquetas/crear" class="dashboard__boton">
+        <i class="fa-solid fa-circle-plus"></i>
+        Nueva etiqueta
+    </a>
+</div>
+>>>>>>> Stashed changes
