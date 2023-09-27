107175bcccc7b4b26b3bae3027547ceb9538bf8a
DarkDucking <segundoleoshadow@gmail.com>
2023-09-27 13:28:22 -0600

    base "conocenos"

---
+++ views/paginas/index.php
@@ -0,0 +1,3 @@
+<main class="studentwebcamp">
+    <h2 class="studentwebcamp__heading"><?php echo $titulo; ?></h2>
+</main>
---
+++ views/paginas/studentwebcamp.php
@@ -0,0 +1,49 @@
+<main class="studentwebcamp">
+    <h2 class="studentwebcamp__heading"><?php echo $titulo; ?></h2>
+    <p class="studentwebcamp__descripcion">La plataforma donde los estudiantes son los docentes</p>
+
+    <div class="studentwebcamp__grid--info">
+        <div class="studentwebcamp__imagen">
+            <picture>
+                <source srcset="build/img/" type="image/">
+                <img loading="lazy" width="200" height="300" src="build/img/student.png" alt="Imagen StudentWebCamp">
+            </picture>
+        </div>
+
+        <div class="studentwebcamp__contenido">
+            <p class="studentwebcamp__texto">Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                Beatae itaque numquam adipisci deleniti doloremque pariatur expedita eum dignissimos? Dolorem ea 
+                sed est nostrum nam excepturi repellat et in esse reprehenderit?</p>
+            <p class="studentwebcamp__texto">Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                Beatae itaque numquam adipisci deleniti doloremque pariatur expedita eum dignissimos? Dolorem ea 
+                sed est nostrum nam excepturi repellat et in esse reprehenderit?</p>
+        </div>
+    </div>
+
+    <div class="studentwebcamp__grid--nosotros">
+        <div class="bloque">
+            <div class="studentwebcamp__imagen">
+                <picture>
+                    <source srcset="build/img/" type="image/">
+                    <img loading="lazy" width="200" height="300" src="build/img/generico.png" alt="Leonardo Cárdenas">
+                </picture>
+            </div>
+            <h3 class="bloque__nombre">Oscar Leonardo Cárdenas Ulloa</h3>
+            <p class="bloque__texto">orem ipsum dolor sit amet consectetur adipisicing elit. 
+                Beatae itaque numquam adipisci deleniti doloremque pariatur expedita eum dignissimos?</p>
+        </div>
+
+        <div class="bloque">
+            <div class="studentwebcamp__imagen">
+                <picture>
+                    <source srcset="build/img/" type="image/">
+                    <img loading="lazy" width="200" height="300" src="build/img/generico.png" alt="Paulina Figueroa">
+                </picture>
+            </div>
+            <h3 class="bloque__nombre">Paulina Michelle Figueroa Noriega</h3>
+            <p class="bloque__texto">orem ipsum dolor sit amet consectetur adipisicing elit. 
+                Beatae itaque numquam adipisci deleniti doloremque pariatur expedita eum dignissimos?</p>
+        </div>
+    </div>
+
+</main>
